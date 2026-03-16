e9d2e029ff08fda4b5f408d8370f15358b31310b
binshad-dm <binshad.p@mediwarehms.com>
2026-03-16 16:34:51 +0530

    floating_draggable_widget removed and added again

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,3 @@
-
 import 'package:floating_draggable_widget/floating_draggable_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -6,7 +5,6 @@ import 'package:section_bar_navigator_system/core/theme/app_theme.dart';
 import 'package:section_bar_navigator_system/core/widgets/index_bar/index_bar_widget.dart';
 import 'package:section_bar_navigator_system/feature/settings/presenter/settings_cubit.dart';
 import 'package:section_bar_navigator_system/feature/settings/presenter/state/settings_state.dart';
-
 
 class App extends StatefulWidget {
   const App({super.key, required this.navigatorKey, this.dx, this.dy});
@@ -26,9 +24,7 @@ class _AppState extends State<App> {
     final size = MediaQuery.of(context).size.shortestSide;
     final mediaQuery = MediaQuery.of(context).size;
     return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => SettingsCubit()),
-      ],
+      providers: [BlocProvider(create: (context) => SettingsCubit())],
       child: MaterialApp(
         navigatorKey: widget.navigatorKey,
         debugShowCheckedModeBanner: false,
@@ -40,31 +36,53 @@ class _AppState extends State<App> {
             : BlocBuilder<SettingsCubit, SettingsState>(
                 builder: (context, state) => FloatingDraggableWidget(
                   resizeToAvoidBottomInset: false,
-                  mainScreenWidget: HomeScreen(isDisableIndexBar: !context.watch<SettingsCubit>().isFloatingScreen),
+                  mainScreenWidget: HomeScreen(
+                    isDisableIndexBar: !context
+                        .watch<SettingsCubit>()
+                        .isFloatingScreen,
+                  ),
                   onDragging: (p0) {
                     isDragging = p0;
                   },
                   onDragEvent: (dx, dy) {
-                    context.read<SettingsCubit>().setfloatingButtonCurrentPosition(dx, dy);
+                    context
+                        .read<SettingsCubit>()
+                        .setfloatingButtonCurrentPosition(dx, dy);
                     if (!isDragging) {
                       print("ds");
                       if (dx >= MediaQuery.of(context).size.width / 2) {
-                        context.read<SettingsCubit>().setHandnessType(HandnessType.right);
+                        context.read<SettingsCubit>().setHandnessType(
+                          HandnessType.right,
+                        );
                       } else {
-                        context.read<SettingsCubit>().setHandnessType(HandnessType.left);
+                        context.read<SettingsCubit>().setHandnessType(
+                          HandnessType.left,
+                        );
                       }
                     }
                   },
-                  dx: context.read<SettingsCubit>().floatingWidgetCurrentDx ?? widget.dx ?? mediaQuery.width - 45,
+                  dx:
+                      context.read<SettingsCubit>().floatingWidgetCurrentDx ??
+                      widget.dx ??
+                      mediaQuery.width - 45,
 
-                  dy: context.read<SettingsCubit>().floatingWidgetCurrentDy ?? widget.dy ?? mediaQuery.height / 2 - 22.5,
+                  dy:
+                      context.read<SettingsCubit>().floatingWidgetCurrentDy ??
+                      widget.dy ??
+                      mediaQuery.height / 2 - 22.5,
 
-                  floatingWidget: context.watch<SettingsCubit>().isFloatingScreen
+                  floatingWidget:
+                      context.watch<SettingsCubit>().isFloatingScreen
                       ? InkWell(
                           onTap: () {
-                            context.read<SettingsCubit>().setNormalScreenState(context);
+                            context.read<SettingsCubit>().setNormalScreenState(
+                              context,
+                            );
                           },
-                          child: Opacity(opacity: 0.5, child: CircleAvatar(child: Text("S"))),
+                          child: Opacity(
+                            opacity: 0.5,
+                            child: CircleAvatar(child: Text("S")),
+                          ),
                         )
                       : SizedBox(),
                   autoAlign: true,
